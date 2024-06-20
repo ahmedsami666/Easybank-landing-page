34524b188af0c67c4c50a06a1f8a85582ac81b10
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-06-20 03:48:27 +0300

    responsive design

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import Easybank from "./components/EasyBank";
 import Features from "./components/Features";
 import Articles from "./components/Articles";
 import Footer from "./components/Footer";
+import HeaderMobile from "./components/HeaderMobile";
+
+var currentWidth = window.innerWidth
+var mobile = false
+if (currentWidth <= 768) {
+  mobile = true
+} else {
+  mobile = false
+}
 
 function App() {
   return (
     <div className="main">
-      <Header />
+      {mobile ? <HeaderMobile /> : <Header />}
       <Intro />
       <Easybank />
       <Features />
---
+++ src/components/HeaderMobile.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const HeaderMobile = () => {
+    return (
+        <div className="header header-mobile">
+        <img src="./images/logo.svg" alt="logo"/>
+        <img src="./images/icon-hamburger.svg" alt="hamburger"/>
+    </div>
+    )
+}
+
+export default HeaderMobile
